07eca3a353e914e5683fb3845cf66e0e37f972b6
gabrielwp <gabriel.piazenski@edu.pucrs.br>
2024-11-13 22:04:05 +0000

    set priority

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+# copy files from host to target
 cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
+#cp $BASE_DIR/../custom-scripts/cod.py $BASE_DIR/target/usr/bin
+#cp $BASE_DIR/../custom-scripts/S42sstf $BASE_DIR/target/etc/init.d
+
+# changing permissions
+#chmod +x $BASE_DIR/target/usr/bin/cod.py
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
+#chmod +x $BASE_DIR/target/etc/init.d/S42sstf
+
+# compiling c code
+$BASE_DIR/host/usr/bin/i686-buildroot-linux-uclibc-gcc custom-scripts/deadline.c -o $BASE_DIR/target/usr/bin/deadline
+$BASE_DIR/host/usr/bin/i686-buildroot-linux-uclibc-gcc custom-scripts/setpriority.c -o $BASE_DIR/target/usr/bin/setpriority
 
 #Compile simple driver 
 make -C $BASE_DIR/../modules/simple_driver/
